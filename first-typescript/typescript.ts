7f5540643256e4c68a7397e4c30d735d5537bacb
Hisham Khaled <hishamk1999@gmail.com>
2023-09-19 20:32:21 +0300

    First TypeScript.

---
+++ first-typescript/typescript.ts
@@ -0,0 +1,11 @@
+let turn: number, winner: boolean, tie: boolean, board: number[];
+
+function init() {
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  turn = 1;
+  tie = false;
+  winner = false;
+  console.log(turn);
+}
+
+init();
